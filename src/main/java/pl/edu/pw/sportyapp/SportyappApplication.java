1273c3d6cc1e70970cd06c9f8e76bb8aa0c0ab71
Rafał Szulejko <rafal.szulejko@gmail.com>
2019-12-18 23:27:41 +0100

    Added initial code generated in Spring Initializer.

---
+++ src/main/java/pl/edu/pw/sportyapp/SportyappApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.sportyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportyappApplication.class, args);
+	}
+
+}
