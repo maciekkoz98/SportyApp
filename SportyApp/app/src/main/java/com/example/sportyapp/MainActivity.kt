705599073ef6d84db89d4c44b969c6cde37c7c86
Maciej Kozłowski <maciekkoz98@gmail.com>
2020-02-24 16:57:51 +0100

    localization in map fragment

--- SportyApp/app/src/main/java/com/example/sportyapp/MainActivity.kt
+++ SportyApp/app/src/main/java/com/example/sportyapp/MainActivity.kt
@@ -12,9 +12,10 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
+import androidx.navigation.ui.AppBarConfiguration as AppBarConfiguration1
+
+const val REQUEST_LOCATION = 2137
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,17 +27,12 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
+        appBarConfiguration = AppBarConfiguration1(
             setOf(
                 R.id.nav_home, R.id.nav_fields, R.id.nav_my_games,
                 R.id.nav_add_game
--- SportyApp/app/src/main/java/com/example/sportyapp/ui/home/HomeFragment.kt
+++ SportyApp/app/src/main/java/com/example/sportyapp/ui/home/HomeFragment.kt
@@ -1,23 +1,38 @@
 package com.example.sportyapp.ui.home
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.sportyapp.R
+import com.example.sportyapp.REQUEST_LOCATION
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var localFAB: FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,13 +48,93 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
 //            textView.text = it
 //        })
         mapFragment.getMapAsync(this)
+        localFAB = root.findViewById(R.id.localFAB)
+        localFAB.setOnClickListener {
+            localize()
+        }
+        val addFAB: FloatingActionButton = root.findViewById(R.id.addFAB)
+        addFAB.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with ADD action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity!!)
         return root
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        val warsaw = LatLng(52.0, 21.0)
+        //TODO set markers in the fields locations!
+        val warsaw = LatLng(52.2297, 21.0122)
         mMap.addMarker(MarkerOptions().position(warsaw).title("Marker in Warsaw"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(warsaw))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(warsaw, 13F))
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    localize()
+                } else {
+                    val noWriteMes = getString(R.string.no_local_permission)
+                    Toast.makeText(
+                        activity,
+                        noWriteMes,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                return
+            }
+            else -> {
+
+            }
+        }
+    }
+
+    private fun localize() {
+        if (ContextCompat.checkSelfPermission(
+                context!!,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION
+            )
+        } else {
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    val img: Drawable?
+                    if (location == null) {
+                        img = ResourcesCompat.getDrawable(
+                            context!!.resources,
+                            R.drawable.ic_location_searching_white_24dp,
+                            null
+                        )
+                    } else {
+                        img = ResourcesCompat.getDrawable(
+                            context!!.resources,
+                            R.drawable.ic_my_location_white_24dp,
+                            null
+                        )
+
+                        val longitude = location.longitude
+                        val latitude = location.latitude
+                        val latLng = LatLng(latitude, longitude)
+                        val cameraPosition =
+                            CameraPosition.builder()
+                                .target(latLng)
+                                .zoom(mMap.cameraPosition.zoom)
+                                .build()
+                        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+                    }
+                    localFAB.setImageDrawable(img)
+                }
+            mMap.isMyLocationEnabled = true
+            mMap.uiSettings.isMyLocationButtonEnabled = false;
+        }
     }
 }
