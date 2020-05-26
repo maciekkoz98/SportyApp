902b26e3d816ad9d22cf69d1e59e15132db436d5
Rafał Szulejko <rafal.szulejko@gmail.com>
2020-05-26 21:22:36 +0200

    Modified GameController integration tests to work with security module.

--- src/test/java/pl/edu/pw/sportyapp/game/controller/GameControllerIntegrationTest.java
+++ src/test/java/pl/edu/pw/sportyapp/game/controller/GameControllerIntegrationTest.java
@@ -2,6 +2,7 @@ package pl.edu.pw.sportyapp.game.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 import com.jayway.jsonpath.JsonPath;
 import com.mongodb.client.MongoClients;
 import org.junit.jupiter.api.AfterEach;
@@ -23,12 +24,18 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import pl.edu.pw.sportyapp.game.dao.Game;
+import pl.edu.pw.sportyapp.user.dao.User;
+import pl.edu.pw.sportyapp.user.security.AppUserRole;
 
 import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @DirtiesContext
 @ExtendWith(SpringExtension.class)
@@ -39,29 +46,42 @@ import static org.assertj.core.api.Assertions.fail;
 @ComponentScan("pl.edu.pw.sportyapp")
 class GameControllerIntegrationTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
     private MongoOperations mongoOperations;
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private User user;
+
     @BeforeEach
     void setUp() {
         mongoOperations = new MongoTemplate(MongoClients.create(), "test");
         mongoOperations.dropCollection("game");
+        mongoOperations.dropCollection("user");
         objectMapper = new ObjectMapper();
+
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+
+        user = User.builder().username("bob").fullname("robert").email("bob@bob.pl").passwordHash("bobob").id(6L).role(AppUserRole.USER)
+                .isEnabled(true).isAccountNonExpired(true).isAccountNonLocked(true).isCredentialsNonExpired(true)
+                .gamesParticipatedIds(Lists.newArrayList()).friendsIds(Lists.newArrayList())
+                .build();
     }
 
     @AfterEach
     void tearDown() {
         mongoOperations.dropCollection("game");
+        mongoOperations.dropCollection("user");
         mongoOperations = null;
         objectMapper = null;
     }
 
     @Test
     void getAllNoGames() throws Exception {
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/game").accept(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/game").with(user(user)).accept(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
         String resultString = result.getResponse().getContentAsString();
 
         assertThat(((Collection) JsonPath.read(resultString, "$")).size()).isEqualTo(0);
@@ -69,13 +89,13 @@ class GameControllerIntegrationTest {
 
     @Test
     void getAllGamesExist() throws Exception {
-        Game g1 = Game.builder().id(1L).facility(2L).build();
-        Game g2 = Game.builder().id(2L).facility(2L).build();
+        Game g1 = Game.builder().id(1L).facility(2L).owner(6L).date(System.currentTimeMillis()*2).duration(300).build();
+        Game g2 = Game.builder().id(2L).facility(2L).owner(6L).date(System.currentTimeMillis()*2).duration(300).build();
 
         mongoOperations.insert(g1, "game");
         mongoOperations.insert(g2, "game");
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/game").accept(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/game").with(user(user)).accept(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
         String resultString = result.getResponse().getContentAsString();
 
         assertThat(((Collection) JsonPath.read(resultString, "$")).size()).isEqualTo(2);
@@ -83,10 +103,12 @@ class GameControllerIntegrationTest {
 
     @Test
     void getOneGameExists() throws Exception {
-        Game g1 = Game.builder().id(1L).facility(2L).build();
+        Game g1 = Game.builder().id(1L).facility(2L).owner(6L).date(System.currentTimeMillis()*2).duration(300).build();
+
         mongoOperations.insert(g1, "game");
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/game/1").accept(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/game/1").with(user(user)).accept(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
         String resultString = result.getResponse().getContentAsString();
 
         int resultId = JsonPath.parse(resultString).read( "$.id");
@@ -96,12 +118,12 @@ class GameControllerIntegrationTest {
 
     @Test
     void getOneGameDoesNotExist() throws Exception {
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/game/1").accept(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isNotFound()).andReturn();
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/game/1").with(user(user)).accept(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isNotFound()).andReturn();
     }
 
     @Test
     void createValidInput() throws Exception {
-        Game g1 = Game.builder().id(1L).facility(2L).build();
+        Game g1 = Game.builder().facility(2L).owner(6L).name("gra").date(System.currentTimeMillis()*2).duration(3600L).build();
         String json = null;
 
         try {
@@ -111,7 +133,9 @@ class GameControllerIntegrationTest {
             fail("JSON parsing error");
         }
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/game").content(json).contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isCreated()).andReturn();
+        mongoOperations.insert(user, "user");
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/game").with(user(user)).content(json).contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isCreated()).andReturn();
 
         assertThat(result.getResponse().getContentAsString()).isEqualTo("1");
     }
@@ -120,13 +144,14 @@ class GameControllerIntegrationTest {
     void createInvalidInput() throws Exception {
         String json = "{\"name\":\"supergra\"}";
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/game").content(json).contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isBadRequest()).andReturn();
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/game").with(user(user)).content(json).contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isBadRequest()).andReturn();
     }
 
     @Test
     void updateValidInput() throws Exception {
-        Game g1 = Game.builder().id(1L).facility(2L).build();
+        Game g1 = Game.builder().id(1L).facility(2L).owner(6L).name("gra").isPublic(true).date(System.currentTimeMillis()*2).duration(2600).build();
         mongoOperations.insert(g1, "game");
+        mongoOperations.insert(user, "user");
 
         g1.setFacility(3L);
 
@@ -139,14 +164,14 @@ class GameControllerIntegrationTest {
             fail("JSON parsing error");
         }
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.put("/game/1").content(json).contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.put("/game/1").with(user(user)).content(json).contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
 
         assertThat(mongoOperations.findById(1, Game.class, "game").getFacility()).isEqualTo(3L);
     }
 
     @Test
     void updateNotExistingGame() throws Exception {
-        Game g1 = Game.builder().id(1L).facility(2L).build();
+        Game g1 = Game.builder().id(1L).facility(2L).owner(6L).name("gra").isPublic(true).date(System.currentTimeMillis()*2).duration(2600).build();
 
         String json = null;
 
@@ -157,7 +182,7 @@ class GameControllerIntegrationTest {
             fail("JSON parsing error");
         }
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.put("/game/1").content(json).contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isNotFound()).andReturn();
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.put("/game/1").with(user(user)).content(json).contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isNotFound()).andReturn();
     }
 
     @Test
@@ -177,20 +202,20 @@ class GameControllerIntegrationTest {
             fail("JSON parsing error");
         }
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.put("/game/1").content(json).contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isBadRequest()).andReturn();
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.put("/game/1").with(user(user)).content(json).contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isBadRequest()).andReturn();
     }
 
     @Test
     void deleteExistingGame() throws Exception {
-        Game g1 = Game.builder().id(1L).facility(2L).build();
+        Game g1 = Game.builder().id(1L).facility(2L).owner(6L).name("gra").isPublic(true).date(System.currentTimeMillis()*2).duration(2600).build();
 
         mongoOperations.insert(g1, "game");
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/game/1")).andExpect(MockMvcResultMatchers.status().isNoContent()).andReturn();
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/game/1").with(user(user))).andExpect(MockMvcResultMatchers.status().isNoContent()).andReturn();
     }
 
     @Test
     void deleteNotExistingGame() throws Exception {
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/game/1")).andExpect(MockMvcResultMatchers.status().isNotFound()).andReturn();
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/game/1").with(user(user))).andExpect(MockMvcResultMatchers.status().isNotFound()).andReturn();
     }
 }
