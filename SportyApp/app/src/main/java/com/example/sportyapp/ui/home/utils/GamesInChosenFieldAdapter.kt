18be04ba2df745be1c38b0703105ba65ebba3197
Maciej Kozłowski <maciekkoz98@gmail.com>
2020-06-09 23:05:36 +0200

    sports icons, left drawer icons

--- SportyApp/app/src/main/java/com/example/sportyapp/ui/home/utils/GamesInChosenFieldAdapter.kt
+++ SportyApp/app/src/main/java/com/example/sportyapp/ui/home/utils/GamesInChosenFieldAdapter.kt
@@ -27,7 +27,7 @@ class GamesInChosenFieldAdapter(private val gamesList: ArrayList<Game>) :
     override fun onBindViewHolder(holder: GamesInChosenFieldHolder, position: Int) {
         val game = gamesList[position]
         holder.linearLayout.event_view_event_name.text = game.name
-        //TODO holder.linearLayout.event_view_sport_icon
+        setSportIcon(holder, game.sport.nameEN)
         holder.linearLayout.event_view_sport_name.text = getSportText(holder, game.sport.nameEN)
         val stamp = Timestamp(game.date)
         val date = Date(stamp.time)
@@ -48,6 +48,18 @@ class GamesInChosenFieldAdapter(private val gamesList: ArrayList<Game>) :
                 game.players.size.toString(),
                 game.maxPlayers.toString()
             )
+    }
+
+    private fun setSportIcon(holder: GamesInChosenFieldHolder, sportName: String) {
+        when (sportName) {
+            "Basketball" -> holder.linearLayout.event_view_sport_icon.setImageResource(R.drawable.ic_basketball_svg)
+            "Football" -> holder.linearLayout.event_view_sport_icon.setImageResource(R.drawable.ic_football)
+            "Volleyball" -> holder.linearLayout.event_view_sport_icon.setImageResource(R.drawable.ic_volleyball)
+            "Handball" -> holder.linearLayout.event_view_sport_icon.setImageResource(R.drawable.ic_handball)
+            "Badminton" -> holder.linearLayout.event_view_sport_icon.setImageResource(R.drawable.ic_badminton)
+            "Tennis" -> holder.linearLayout.event_view_sport_icon.setImageResource(R.drawable.ic_tennis)
+            "Table tennis" -> holder.linearLayout.event_view_sport_icon.setImageResource(R.drawable.ic_table_tennis)
+        }
     }
 
     private fun getSportText(holder: GamesInChosenFieldHolder, sportName: String): String {
